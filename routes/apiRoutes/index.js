6386a104a704b1c949597735bcef87c0e2a1a44f
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-11-14 23:02:26 -0600

    Refactor routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { notes } = require("../../data/notes.json");
-const {createNewNote, deleteNote, findById} = require('../../lib/notes.js');
+const {assignId, createNewNote, deleteNote, findById} = require('../../lib/notes.js');
 const fs = require("fs");
 const path = require("path");
 
@@ -10,28 +10,14 @@ router.get("/notes", function (request, response) {
 });
 
 router.post('/notes', function (request, response) {
-    request.body.id = -1;
-    let escape = false;
-    while(!escape) {
-        request.body.id++;
-        if (notes.findIndex(function (element) {return element.id == request.body.id}) === -1) {
-            escape = true;
-        }
-    }
-    request.body.id = request.body.id.toString();
+    request.body.id = assignId(request.body.id, notes);
     const note = createNewNote(request.body, notes);
     response.json(note);
 });
 
 router.delete('/notes/:id', function (request, response) {
-    const id = request.params.id;
-    let index = notes.findIndex(function (element) { return element.id == id});
-    notes.splice(index, 1);
-    fs.writeFileSync(
-        path.join(__dirname, '../../data/notes.json'),
-        JSON.stringify({ notes: notes }, null, 2)
-    );
-    response.json(request.body);
+    const note = deleteNote(request.body, request.params.id, notes);
+    response.json(note);
 });
 
 module.exports = router;
