f829272c91e7d214b9cb6fa6580555a425c653c9
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-11-14 20:26:04 -0600

    Add post route and refactor structure

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,9 +1,16 @@
 const router = require("express").Router();
 const { notes } = require("../../data/notes.json");
+const createNewNote = require('../../lib/notes.js');
 
 router.get("/notes", function (request, response) {
     let results = notes;
     response.json(results);
+});
+
+router.post('/notes', function (request, response) {
+    request.body.id = notes.length.toString();
+    const note = createNewNote(request.body, notes);
+    response.json(note);
 });
 
 module.exports = router;
